bc549f79101aad6e6bc4e41d134bfc00485307ae
jo631 <52406729+jo631@users.noreply.github.com>
2020-08-14 18:29:23 +0900

    11724 end

--- 11724_Count_of_Connected/answer.cpp
+++ 11724_Count_of_Connected/answer.cpp
@@ -29,8 +29,43 @@ int main() {
 
 	//----------- 입력값을 받는 단계
 
+	//BFS , visit이용해서 중복제거
 
+
+
+	//visit[0] = true;
+
+	vector<bool> visit(n, false);
+	int cnt{}; // 모든 노드를 돌았는지 체크하는 변수
+	int result{}; // 정답을 저장하는 변수
+
+	while (cnt < n) { //모든 노드를 돌때까지
+
+		queue<int> q;
+		for (int i = 0; i < n; i++) { // 큐에 첫번째 넣을 인덱스를 설정해주는 단계
+			if (!visit[i]) {
+				q.push(i); visit[i] = true;
+				break; // 들린적이 없을 때 큐에 첫번째로 넣어주고 for문 탈출 ( 첫번쨰는 무조건 0)
+			}
+		}
+
+		while (!q.empty()) { // 연결되어있는 노드들을 탐색
+			int temp = q.front();
+			cnt++; // 노드를 들렸을 때 수를 1 증가
+			q.pop();
+
+			for (int i = 0; i < n; i++) {
+				if (input[temp][i] == 1 && temp != i && !visit[i]) { // 이어져있고, 자기자신이 아니고, 들린적이 없으면
+					q.push(i);
+					visit[i] = true;
+				}
+			}
+		}
+
+		result++; // 큐가 비었을 때(더이상 연결되어있는 노드가 없을 때) 1 추가
+	}
 	
+	cout << result;
 
 	return 0;
 }
