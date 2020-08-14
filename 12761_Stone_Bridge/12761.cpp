845e47b0c5cf08044820ed0044e5dfcc92500ed2
jo631 <52406729+jo631@users.noreply.github.com>
2020-08-15 01:18:23 +0900

    Update 12761.cpp

--- 12761_Stone_Bridge/12761.cpp
+++ 12761_Stone_Bridge/12761.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
-#include <vector>
 #include <queue>
 
 using namespace std;
 
 /*
-	방법
+	pair를 만들어서 현재위치와 cnt 저장
+	bfs쓰면 풀리지 않을까?
+	재귀쓰면 메모리터질거같은데
+*/
+
+/*
+	8가지 방법
 	+1
 	-1
 	A만큼 + 이동
@@ -16,17 +21,12 @@ using namespace std;
 	현위치 * B
 */
 
-/*
-	pair를 만들어서 현재위치와 cnt 저장
-	bfs쓰면 풀리지 않을까?
-	재귀쓰면 메모리터질거같은데
-*/
-
 int main() {
 	
 	int A{}, B{}, start{}, end{};
 	cin >> A >> B >> start >> end;
 	//-----------------입력값을 받는 단계
+	if (start == end) { cout << 0; } //설마?
 
 	queue<pair<int, int>> q; //왼쪽:위치 오른쪽:카운트
 	q.push(make_pair(start,0));
@@ -35,6 +35,32 @@ int main() {
 		int location = q.front().first;
 		int cnt = q.front().second+1; //호출하면서 카운트를 1 늘림
 		q.pop();
+
+		if (location * A < end) { q.push(make_pair(location * A, cnt)); } // *A
+		else if (location * A == end) { cout << cnt; break; }
+
+		if (location * B < end) { q.push(make_pair(location * B, cnt)); } // *B
+		else if (location * B == end) { cout << cnt; break; }
+
+		if (location + A < end) { q.push(make_pair(location + A, cnt)); }
+		else if (location + A == end) { cout << cnt; break; } // +A할땐 end보다 초과할수가 있음, 이럴 땐 제외
+
+		if (location + B < end) { q.push(make_pair(location + B, cnt)); } // +B
+		else if (location + B == end) { cout << cnt; break; }
+
+		
+		if (location + 1 < end) { q.push(make_pair(location + 1, cnt)); }
+		else { cout << cnt; break; } // +1일 떈 초과할일이 없다
+
+		if (location - A > start) { q.push(make_pair(location - A, cnt)); } // -A
+		if (location - B > start) { q.push(make_pair(location - B, cnt)); } // -B
+		if (location - 1 > start) { q.push(make_pair(location - 1, cnt)); } // -1 뒤로갔는데 end에 갈일은 없다
+
+
+		
+
+
+
 	}
 
 	return 0;
