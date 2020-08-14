229f032ccd844ac261c9b357c8145eb48561792a
jo631 <52406729+jo631@users.noreply.github.com>
2020-08-15 00:54:01 +0900

    ing

--- 12761_Stone_Bridge/12761.cpp
+++ 12761_Stone_Bridge/12761.cpp
@@ -4,7 +4,38 @@
 
 using namespace std;
 
+/*
+	방법
+	+1
+	-1
+	A만큼 + 이동
+	A만큼 - 이동
+	B만큼 + 이동
+	B만큼 - 이동
+	현위치 * A
+	현위치 * B
+*/
+
+/*
+	pair를 만들어서 현재위치와 cnt 저장
+	bfs쓰면 풀리지 않을까?
+	재귀쓰면 메모리터질거같은데
+*/
+
 int main() {
+	
+	int A{}, B{}, start{}, end{};
+	cin >> A >> B >> start >> end;
+	//-----------------입력값을 받는 단계
+
+	queue<pair<int, int>> q; //왼쪽:위치 오른쪽:카운트
+	q.push(make_pair(start,0));
+
+	while (true) {
+		int location = q.front().first;
+		int cnt = q.front().second+1; //호출하면서 카운트를 1 늘림
+		q.pop();
+	}
 
 	return 0;
 }
