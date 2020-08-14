b34720329abb197f588ed0b1f8bae46efdf488f3
jo631 <52406729+jo631@users.noreply.github.com>
2020-08-14 17:16:23 +0900

    입력값 받기 완료

--- 11724_Count_of_Connected/answer.cpp
+++ 11724_Count_of_Connected/answer.cpp
@@ -1,22 +1,36 @@
 #include <iostream>
 #include <vector>
 
+#include <queue>
 using namespace std;
 
 int main() {
 	int n{}, m{};
-
 	cin >> n >> m;
-	vector<pair<int, int>> input;
+
+	vector<vector<int>> input(n, vector<int>(n,0));
+	
+	for (int i = 0; i < n; i++) { // 자기 자신 연결(1)
+		input[i][i] = 1;
+	}
+
+	// 이어진곳에 1
+	// 1 1 0
+	// 1 1 0 
+	// 0 0 1  (예를들어)
 	for (int i = 0; i < m; i++) {
-		int n1, n2;
+		int n1{}, n2{};
 		cin >> n1 >> n2;
-		input.emplace_back(n1, n2);
+		
+		input[n1-1][n2-1] = 1;
+		input[n2-1][n1-1] = 1;
 	}
-	//-----------
-	for (auto i : input) {
-		cout << i.first << i.second << endl;
-	}
+
+
+	//----------- 입력값을 받는 단계
+
+
+	
 
 	return 0;
 }
