7eb24350fbebc81c4dafebc169a8935626a4950e
jo631 <52406729+jo631@users.noreply.github.com>
2020-08-19 19:21:13 +0900

    Update 2517.cpp

--- 2517_Running/2517.cpp
+++ 2517_Running/2517.cpp
@@ -7,15 +7,15 @@ int main() {
 	int num{};
 	cin >> num;
 
-	vector<int> numbers{};
+	vector<pair<int,int>> numbers{};
 	int temp{};
 	for (int i = 0; i < num; i++) {
 		cin >> temp;
-		numbers.push_back(temp);
+		numbers.emplace_back(temp,i+1);
 	}
 
 	for (auto i : numbers) {
-		cout << i << endl;
+		cout << i.first << "\t" << i.second << endl;
 	}
 
 	return 0;
