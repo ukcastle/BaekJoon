223038203732deb8bd25f2f20ea54a4628970165
jo631 <52406729+jo631@users.noreply.github.com>
2020-08-22 21:27:06 +0900

    Update 2517.cpp

--- 2517_Running/2517.cpp
+++ 2517_Running/2517.cpp
@@ -14,20 +14,5 @@ int main() {
 		numbers.emplace_back(temp,i+1);
 	}
 
-	for (int i = 1; i < num; i++) {
-		for (int j = 0; j < i; j++) {
-			if (numbers[i].first > numbers[j].first) {
-				if (numbers[i].second > 1) {
-					numbers[i].second--;
-				}
-			}
-			if (numbers[i].second == 1) { break; }
-		}
-	}
-
-	for (auto i : numbers) {
-		cout << i.second << endl;
-	}
-
 	return 0;
 }
