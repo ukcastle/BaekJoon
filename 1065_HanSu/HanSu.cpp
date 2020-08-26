fa5b139d2ecb3e351070a46470dfc67ce42451d4
jo631 <52406729+jo631@users.noreply.github.com>
2020-08-26 18:23:18 +0900

    dd

--- 1065_HanSu/HanSu.cpp
+++ 1065_HanSu/HanSu.cpp
@@ -3,6 +3,10 @@ using namespace std;
 
 int main() {
 
+		ios_base::sync_with_stdio(false);
+		cin.tie(nullptr);
+		cout.tie(nullptr);
+
 	int input{};
 	cin >> input;
 	//----------------입력값을 받는 단계
