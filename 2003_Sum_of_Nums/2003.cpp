5294dfcf60a11d645f85b26c432b5fbacb52adac
jo631 <52406729+jo631@users.noreply.github.com>
2020-08-27 00:21:21 +0900

    Update 2003.cpp

--- 2003_Sum_of_Nums/2003.cpp
+++ 2003_Sum_of_Nums/2003.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
 	register int nums[10000];
-	
+	register int answer{};
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
@@ -11,14 +11,37 @@ int main() {
 	int n, target;
 	cin >> n >> target;
 	
-	for (int i = 0; i < n; i++) {
-		cin >> nums[i];
-	}
+	register int num;
+	int searchIndex{};
+
+	cin >> nums[0];
+	if (nums[0] == target) { answer++; }
+
+	for (int i = 1; i < n; i++) {
+		cin >> num;
+		nums[i] = nums[i-1] + num;
+		if (nums[i] == target) { answer++; }
+	} // 합을 저장
+
+	//-------------------------------값을 받는 단계
+
 
 	for (int i = 0; i < n; i++) {
-		cout << nums[i];
+		if (nums[i] >= target) {
+			searchIndex = i;
+			break;
+		}
 	}
+
+	for (int i = searchIndex; i < n; i++) {
+		for (int j = 0; j < i; j++) {
+			if (nums[i] - nums[j] == target) {
+				answer++;
+				break;
+			}
+		}
+	}
+	cout << answer;
 
 	return 0;
-
 }
