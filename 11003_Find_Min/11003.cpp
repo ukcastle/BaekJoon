4346f15f06e4f9f6c2eb6d837bd8bcf5287ba022
jo631 <52406729+jo631@users.noreply.github.com>
2020-08-27 17:05:25 +0900

    Update 11003.cpp

--- 11003_Find_Min/11003.cpp
+++ 11003_Find_Min/11003.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int nums[5000000];
-
+//queue를 이용한 슬라이딩 윈도우 사용해라
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
@@ -12,11 +12,14 @@ int main() {
 	int n, l;
 	cin >> n >> l;
 
-	for (int i = 0; i < n; i++) {
+	for (int i = 0; i < l-1; i++) {
 		cin >> nums[i];
+		cout << *min_element(nums, nums + i) << " ";
 	}
-	for (int i = 2; i < n; i++) {
-		cout << *min_element(nums + i - 2, nums + i) << " "; //대충이거이용해서
+	
+	for (int i = l-1; i < n; i++) {
+		cin >> nums[i];
+		cout << *min_element(nums + i - l + 1, nums + i + 1) << " ";
 	}
 	return 0;
 }
