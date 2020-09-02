c9226d1940d763ae553c5ca6e056b372f793d473
jo631 <52406729+jo631@users.noreply.github.com>
2020-09-02 21:51:30 +0900

    Update 2096.cpp

--- 2096_goDown/2096.cpp
+++ 2096_goDown/2096.cpp
@@ -13,30 +13,24 @@ int main() {
 	cin >> n;
 
 	int l, c, r;
-	/*
-	cin >> l >> c >> r;
-	maxNum[0] = minNum[0] = l;
-	maxNum[1] = minNum[1] = c;
-	maxNum[2] = minNum[2] = r;
-	*/
-
+	
 	for (int i = 0; i < n; i++) {
 		cin >> l >> c >> r;
-		int tempL = max(l, c);
-		int tempC = max(max(l, c), r);
-		int tempR = max(c, r);
+		int tempL = max(maxNum[0] + l, maxNum[0] + c);
+		int tempC = max(max(maxNum[1] + l, maxNum[1] + c), maxNum[1] + r);
+		int tempR = max(maxNum[2] + c, maxNum[2] + r);
 
-		maxNum[0] += tempL;
-		maxNum[1] += tempC;
-		maxNum[2] += tempR;
+		maxNum[0] = tempL;
+		maxNum[1] = tempC;
+		maxNum[2] = tempR;
 
-		tempL = min(l, c);
-		tempC = min(min(l, c), r);
-		tempR = min(c, r);
+		tempL = min(minNum[0] + l, minNum[0] + c);
+		tempC = min(min(minNum[1] + l, minNum[1] + c), minNum[1] + r);
+		tempR = min(minNum[2] + c, minNum[2] + r);
 
-		minNum[0] += tempL;
-		minNum[1] += tempC;
-		minNum[2] += tempR;
+		minNum[0] = tempL;
+		minNum[1] = tempC;
+		minNum[2] = tempR;
 	}
 
 
