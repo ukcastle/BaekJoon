1d6f9719966daa1eb54860b920a6044bb939c98d
jo631 <52406729+jo631@users.noreply.github.com>
2020-09-02 20:18:44 +0900

    Update 2096.cpp

--- 2096_goDown/2096.cpp
+++ 2096_goDown/2096.cpp
@@ -1,13 +1,26 @@
 #include <iostream>
 using namespace std;
+
+struct num
+{
+	int left;
+	int center;
+	int right;
+	int index;
+};
+
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
+
+
 	int n{};
 	cin >> n;
 	
+
+
 
 	return 0;
 }
