5a2f2f6a0a01d3f161913eadccf65790f656e3ba
jo631 <52406729+jo631@users.noreply.github.com>
2020-09-10 18:43:41 +0900

    1991 finish

--- 1991_Tree_Circuit/1991.cpp
+++ 1991_Tree_Circuit/1991.cpp
@@ -3,30 +3,47 @@ using namespace std;
 char tree[26][3]; //[n][0] 본인 [1]왼쪽 [2]오른쪽
 
 void preOrder(char root[3]) {
-	if (root[0] == '.') { return; }
-	cout << root[0];
-	preOrder(tree[root[1] - 'A']);
-	preOrder(tree[root[2] - 'A']);
+	if (root[0] <= '.') { return; }
+	else {
+		cout << root[0];
+		preOrder(tree[root[1] - 'A']);
+		preOrder(tree[root[2] - 'A']);
+	}
+}
+
+void inOrder(char root[3]) {
+	if (root[0] <= '.') { return; }
+	else {
+		inOrder(tree[root[1] - 'A']);
+		cout << root[0];
+		inOrder(tree[root[2] - 'A']);
+	}
+}
+
+void postOrder(char root[3]) {
+	if (root[0] <= '.') { return; }
+	else {
+		postOrder(tree[root[1] - 'A']);
+		postOrder(tree[root[2] - 'A']);
+		cout << root[0];
+	}
 }
 
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
 	int n{};
 	cin >> n;
 
-	int temp,left,right;
+	char temp;
 	for (int i{0}; i < n; i++) {
-		cin >> temp>>left>>right;
+		cin >> temp;
 		tree[temp - 'A'][0] = temp;
-		tree[temp - 'A'][1] = left;
-		tree[temp - 'A'][2] = right;
-		
+		cin >> tree[temp - 'A'][1] >> tree[temp - 'A'][2];
 	}
 	
-	preOrder(tree[0]);
+	preOrder(tree[0]); cout << endl;
+	inOrder(tree[0]); cout << endl;
+	postOrder(tree[0]); cout << endl;
 
 	return 0;
 }
