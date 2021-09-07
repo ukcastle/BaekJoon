9c1771d89a46d7e32c05976eb758fd795cb095d1
ukcastle <jo631@naver.com>
2021-09-07 20:49:18 +0900

    change 2303

--- 2303_Number_Game/test_case.py
+++ 2303_Number_Game/test_case.py
@@ -1,10 +1,11 @@
-testList = []
+class TestCase(list):
 
-def appendTest(q,a):
-    testList.append((q,a))
+  def __init__(self):
+    self.addTestCase()
 
-appendTest(q=['3','7 5 5 4 9','1 1 1 1 1','2 3 3 2 10'], a=1)
-appendTest(q=['2','1 1 1 1 1','1 1 1 1 1'], a=2)
-
-def getTestList():
-    return testList
+  def appendTest(self,q,a):
+    self.append((q,a))
+  
+  def addTestCase(self):
+    self.appendTest(q=['3','7 5 5 4 9','1 1 1 1 1','2 3 3 2 10'], a=1)
+    self.appendTest(q=['2','1 1 1 1 1','1 1 1 1 1'], a=2)
